d20332bd75c79d7a1f41793ba448aadc12711e9d
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-08-11 01:19:58 +0200

    fix: fix import module issue

--- bidi-client/src/app/dashboard/dashboard.module.ts
+++ bidi-client/src/app/dashboard/dashboard.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class DashboardModule { }
